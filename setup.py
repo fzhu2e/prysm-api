90eb1f9cc17ad5d92e538b5c6c818384322e343b
Feng Zhu <lyricorpse@gmail.com>
2019-09-25 12:25:18 -0700

    Change ver for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='prysm-api',
-    version='0.5.0',
+    version='0.5.1',
     description="The API for PRoxY System Modeling (PRYSM)",
     long_description=long_description,
     long_description_content_type="text/markdown",
